5fc22d2ddced438328437307e2086dc88eec9215
Ziyosh <ziyodaakhmedova1998@gmail.com>
2022-07-16 17:44:24 +0300

    My second commit again!

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -4,7 +4,7 @@ public class PrintWordsApp {
     public static void main(String[] args) {
         //write your code here
         // "java" + \n "is" + \n +"smth".
-        System.out.println("\"java\" + \\n \"is\" + \\n +\"smth\".");
+        System.out.println("java" + "\nis" + "\nsmth");
 
     }
 }
